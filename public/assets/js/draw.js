2665ba7728e38e2b81387e1e4bf2cf5b78174222
dapmas <sampadmedda@gmail.com>
2019-07-18 11:23:49 +0530

    create drawing surface

---
+++ public/assets/js/draw.js
@@ -0,0 +1,37 @@
+const canvas = document.getElementById('myCanvas');
+const c = canvas.getContext('2d');
+const paintingSurface = document.getElementById('content');
+const paintStyle = getComputedStyle(paintingSurface);
+
+
+
+canvas.width = parseInt(paintStyle.getPropertyValue('width'));
+canvas.height = parseInt(paintStyle.getPropertyValue('height'));
+
+
+let mouse = {x: 0, y: 0};
+
+canvas.addEventListener('mousemove', function(e) {
+  mouse.x = e.pageX - this.offsetLeft;
+  mouse.y = e.pageY - this.offsetTop;
+}, false);
+
+c.lineWidth = 3;
+c.lineJoin = 'round';
+c.lineCap = 'round';
+c.strokeStyle = '#FF0000';
+
+canvas.addEventListener('mousedown', function(e) {
+  c.beginPath();
+  c.moveTo(mouse.x, mouse.y);
+  canvas.addEventListener('mousemove', onPaint, false);
+}, false);
+
+canvas.addEventListener('mouseup', function (e) {
+  canvas.removeEventListener('mousemove', onPaint, false);
+}, false);
+
+let onPaint = function() {
+  c.lineTo(mouse.x, mouse.y);
+  c.stroke();
+}
