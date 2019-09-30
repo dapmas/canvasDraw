3db3f50f3a33634f5c80660e1e18e30638b1a4d7
dapmas <sampadmedda@gmail.com>
2019-09-30 06:31:53 +0530

    annotate on the video

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,9 @@ module.exports = {
     "env": {
         "browser": true,
         "commonjs": true,
-        "es6": true
-    },
+        "es6": true,
+        "jquery": true
+      },
     "extends": "eslint:recommended",
     "globals": {
         "Atomics": "readonly",
--- public/assets/js/canvas.js
+++ public/assets/js/canvas.js
@@ -1,66 +1,79 @@
-// const canvasContainer = document.querySelector('div.canvas-container');
-// const video = document.getElementById("vd1");
-// const videoControl = document.getElementById("video-control");
-// const inkStart = document.getElementById("ink-start");
-// const inkStop = document.getElementById("ink-stop");
-// const inkClear = document.getElementById("ink-clear");
+// const canvasContainer = $('div.canvas-container');
+const video = $("#vd1").get(0);
+const videoControl = $("#video-control");
+const inkStart = $("#ink-start");
+const inkStop = $("#ink-stop");
+const inkClear = $("#ink-clear");
 
-// const mouse = {x: 0, y: 0};
+const mouse = {x: 0, y: 0};
 
-
-// window.onload = function() {
-//   init();
-//   window.addEventListener('resize', init, false);
-// }
-
-// function init() {
-//   const canvas = document.getElementById("videoCanvas");
-//   const c = canvas.getContext("2d");
-//   /* If you want to use the full window as the canvas **/
-//   // const canvasWidth = window.innerWidth - 5; /** except scrollbars **/
-//   // const canvasHeight = window.innerHeight - 5; /** except scrollbars **/
-
-//   /** If you want to use only the height and width of the parent div **/
-//   /** In Jquery **/
-//   // const canvasWidth = canvasContainer.width();
-//   // const canvasHeight = canvasContainer.height();
-
-//   /** In normal js **/
-//   const canvasWidth = canvasContainer.offsetWidth;
-//   const canvasHeight = canvasContainer.offsetHeight;
-
-//   c.canvas.width = canvasWidth;
-//   c.canvas.height = canvasHeight;
-
-//   videoControl.addEventListener('click', function () {
-//     if (!video.paused) video.pause();
-//     else video.play();
-//   }, false);
-
-//   canvas.addEventListener('mousemove', function(e) {
-//     mouse.x = e.pageX - this.offsetLeft;
-//     mouse.y = e.pageY - this.offsetTop;
-//   }, false);
-
-//   c.lineWidth = 3;
-//   c.lineJoin = 'round'; 
-//   c.lineCap = 'round';
-//   c.strokeStyle = '#FF0000';
-
-//   canvas.addEventListener('mousedown', function() {
-//     c.beginPath();
-//     c.moveTo(mouse.x, mouse.y);
-//     canvas.addEventListener('mousemove', onPaint, false);
-//   }, false);
+$(document).ready(function() {
+  init();
+  $(window).on('resize', init);
+});
 
 
-//   canvas.addEventListener('mouseup', function () {
-//     canvas.removeEventListener('mousemove', onPaint, false);
-//   }, false);
+function init() {
+  const canvas = $("#videoCanvas");
+  const c = canvas.get(0).getContext("2d");
+  // const canvasWidth = canvasContainer.width();
+  // const canvasHeight = canvasContainer.height();
+  // c.canvas.width = canvasWidth;
+  // c.canvas.height = canvasHeight;
 
-//   let onPaint = function() {
-//     c.lineTo(mouse.x, mouse.y);
-//     c.stroke();
-//   }
+  // in actual production canvas will be height and width of the video container
+  c.canvas.width = video.videoWidth;
+  c.canvas.height= video.videoHeight;
 
-// }
+  videoControl.on('click', function () {
+    if (!video.paused) video.pause();
+    else video.play();
+  });
+
+  canvas.on('mousemove', function(e) {
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+    // console.log(`${mouse.x}, ${mouse.y}`);
+  });
+
+  c.lineWidth = 3;
+  c.lineJoin = 'round'; 
+  c.lineCap = 'round';
+  c.strokeStyle = '#FF0000';
+
+  inkStart.on('click', function() {
+    console.log("start inking...");
+    inkStart.attr('disabled', true);
+    canvas.css({cursor : 'crosshair'});
+    canvas.on('mousedown', onDraw);
+
+    canvas.on('mouseup mouseleave', function () {
+      canvas.off('mousemove', onPaint);
+    });
+  });
+
+  inkStop.on('click', function(){
+    console.log('stop inking...');
+    canvas.css({cursor : 'default'});
+    canvas.off('mousedown', onDraw);  
+    inkStart.attr('disabled', false);
+  });
+
+  inkClear.on('click', function() {
+    console.log('clearing the canvas...');
+    canvas.css({cursor : 'default'});
+    canvas.off('mousemove', onPaint);
+    c.clearRect(0, 0, c.canvas.width, c.canvas.height);
+  });
+
+  let onPaint = function() {
+    c.lineTo(mouse.x, mouse.y);
+    c.stroke();
+  }
+
+  let onDraw = function() {
+    c.beginPath();
+    c.moveTo(mouse.x, mouse.y);
+    canvas.on('mousemove', onPaint);
+  }
+}
